6b6aa20792dc03eb713605efbf9395ce61c828c2
Aswanth-s-382 <110896995+Aswanth-s-382@users.noreply.github.com>
2022-08-24 15:10:01 +0530

    Add files via upload

---
+++ allsunday.py
@@ -0,0 +1,13 @@
+from datetime import date,timedelta
+
+def all_sundays(year):
+    dt=date(year,1,1)
+    print("________")
+    print(dt.weekday())
+    dt +=timedelta(days=6 - dt.weekday())
+    while dt.year==year:
+        yield dt
+        dt +=timedelta(days=7)
+
+for s in all_sundays(2020):
+    print(s)
